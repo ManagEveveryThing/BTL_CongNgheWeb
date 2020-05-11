bd2bc94544c6537361fb79595aa80845d0258e59
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2020-05-11 21:21:49 +0700

    update
    update

--- BTL_CNW/Admin/Contents/js/homeController.js
+++ BTL_CNW/Admin/Contents/js/homeController.js
@@ -1,18 +1,28 @@
-﻿
-$(document).ready(function () {
-    $(".ajax_selected_click").change(function () {
-        var nameTb = $(".ajax_selected_click option:selected").text();
+﻿$(document).ready(function () {
+    $(".ajax_editRow").click(function () {
+        var nameTable = $(".ajax_selected_click option:selected").text();
+        var key1 = $(this).closest("tr").find(".key1").text();
+        var key2 = $(this).closest("tr").find(".key2").text();
+        //var $row = $(this).parent().parent();
+        //var $row = $(this).closest("tr");
+        //var $tds = $row.find("td");
+        //var rowval = ""
+        //$.each($tds, function () {
+        //    rowval = rowval+($(this).text());
+        //});
         var dataModel = $('#dataTable');
         $.ajax({
-            url: '/Home/getTableData/' + nameTb,
-                contentType: 'application/html ; charset:utf-8',
-                data: { tableName: nameTb },
-                type: 'GET',
-                dataType: 'html',
-                success: function (result) { dataModel.empty().append(result); }
-            });
+            url: '/Home/ShowRow/',
+            contentType: 'application/html ; charset:utf-8',
+            data: { tableName: nameTable, key1: key1,key2: key2 },
+            type: 'GET',
+            dataType: 'html',
+            success: function (result) { dataModel.empty().append(result); }
+        });
     });
-    $(".ajax_editRow").click(function () {
+
+
+    $(".ajax_selected_click").change(function () {
         var nameTb = $(".ajax_selected_click option:selected").text();
         var dataModel = $('#dataTable');
         $.ajax({
@@ -24,5 +34,5 @@ $(document).ready(function () {
             success: function (result) { dataModel.empty().append(result); }
         });
     });
+    
 });
-
--- BTL_CNW/Admin/Controllers/HomeController.cs
+++ BTL_CNW/Admin/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ namespace Admin.Controllers
         
         public ActionResult Table()
         {
-            var model = context.DestinationReviews.ToList();
+            var ads = context.DestinationReviews.ToList();
             var tableNameList = context.TenCacBangs.ToList();
             ViewBag.tableNameList = tableNameList;
-            return View(model);
+            return View(ads);
         }
         [ChildActionOnly]
         public ActionResult _PartialPage_TableData(string nametable)
@@ -152,9 +152,155 @@ namespace Admin.Controllers
             return PartialView("_TableDB", data);
         }
 
+        
+        
         [HttpGet]
-        public ActionResult getOneRow(string tableName)
+        public ActionResult ShowRow(string tableName, string key1,string key2)
         {
+            ViewBag.nameTable = tableName;
+            ViewBag.colName = context.Database.SqlQuery<colName>("select * from colName where TABLE_NAME = '" + tableName + "'").ToList();
+            object data;
+            switch (tableName)
+            {
+                case "Blog":
+                    data = context.Database.SqlQuery<Blog>("select * from " + tableName +" where maBlog = "+key1).ToList();
+                    break;
+                case "Customer":
+                    data = context.Database.SqlQuery<Customer>("select * from " + tableName + " where maBlog = " + key1).ToList();
+
+                    break;
+                case "DestinationReview":
+                    data = context.Database.SqlQuery<DestinationReview>("select * from " + tableName + " where maBlog = " + key1).ToList();
+
+                    break;
+                case "DSDatXe":
+                    data = context.Database.SqlQuery<DSDatXe>("select * from " + tableName + " where maBlog = " + key1).ToList();
+
+                    break;
+                case "DSKSCanTT":
+                    data = context.Database.SqlQuery<DSKSCanTT>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "DSKSTheoTrip":
+                    data = context.Database.SqlQuery<DSKSTheoTrip>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "DSKSTrongWL":
+                    data = context.Database.SqlQuery<DSKSTrongWL>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "DSTourCanTT":
+                    data = context.Database.SqlQuery<DSTourCanTT>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "DSTourTrongWL":
+                    data = context.Database.SqlQuery<DSTourTrongWL>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "DSTripTheoTour":
+                    data = context.Database.SqlQuery<DSTripTheoTour>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "ElecBill":
+                    data = context.Database.SqlQuery<ElecBill>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "HomeStay":
+                    data = context.Database.SqlQuery<HomeStay>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "Nation":
+                    data = context.Database.SqlQuery<Nation>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "Province":
+                    data = context.Database.SqlQuery<Province>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "Taxi":
+                    data = context.Database.SqlQuery<Taxi>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "Tour":
+                    data = context.Database.SqlQuery<Tour>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "TourDestination":
+                    data = context.Database.SqlQuery<TourDestination>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "Trip":
+                    data = context.Database.SqlQuery<Trip>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                case "WishList":
+                    data = context.Database.SqlQuery<WishList>("select * from " + tableName + " where maBlog = " + key1).ToList();
+                    break;
+                default:
+                    data = null;
+                    break;
+            }
+            return PartialView("_Eachrow");
+        }
+
+        [HttpGet]
+        public ActionResult ModifyRow(string tableName,Array rowVal)
+        {
+            
+            object data;
+            switch (tableName)
+            {
+                case "Blog":
+                    data = context.Database.SqlQuery<Blog>("select * from " + tableName).ToList();
+                    
+                break;
+                case "Customer":
+                    data = context.Database.SqlQuery<Customer>("select * from " + tableName).ToList();
+                    
+                break;
+                case "DestinationReview":
+                    data = context.Database.SqlQuery<DestinationReview>("select * from " + tableName).ToList();
+               
+                break;
+                case "DSDatXe":
+                    data = context.Database.SqlQuery<DSDatXe>("select * from " + tableName).ToList();
+                
+                break;
+                case "DSKSCanTT":
+                    data = context.Database.SqlQuery<DSKSCanTT>("select * from " + tableName).ToList();
+                break;
+                case "DSKSTheoTrip":
+                    data = context.Database.SqlQuery<DSKSTheoTrip>("select * from " + tableName).ToList();
+                break;
+                case "DSKSTrongWL":
+                    data = context.Database.SqlQuery<DSKSTrongWL>("select * from " + tableName).ToList();
+                break;
+                case "DSTourCanTT":
+                    data = context.Database.SqlQuery<DSTourCanTT>("select * from " + tableName).ToList();
+                break;
+                case "DSTourTrongWL":
+                    data = context.Database.SqlQuery<DSTourTrongWL>("select * from " + tableName).ToList();
+                break;
+                case "DSTripTheoTour":
+                    data = context.Database.SqlQuery<DSTripTheoTour>("select * from " + tableName).ToList();
+                break;
+                case "ElecBill":
+                    data = context.Database.SqlQuery<ElecBill>("select * from " + tableName).ToList();
+                break;
+                case "HomeStay":
+                    data = context.Database.SqlQuery<HomeStay>("select * from " + tableName).ToList();
+                break;
+                case "Nation":
+                    data = context.Database.SqlQuery<Nation>("select * from " + tableName).ToList();
+                break;
+                case "Province":
+                    data = context.Database.SqlQuery<Province>("select * from " + tableName).ToList();
+                break;
+                case "Taxi":
+                    data = context.Database.SqlQuery<Taxi>("select * from " + tableName).ToList();
+                break;
+                case "Tour":
+                    data = context.Database.SqlQuery<Tour>("select * from " + tableName).ToList();
+                break;
+                case "TourDestination":
+                    data = context.Database.SqlQuery<TourDestination>("select * from " + tableName).ToList();
+                break;
+                case "Trip":
+                    data = context.Database.SqlQuery<Trip>("select * from " + tableName).ToList();
+                break;
+                case "WishList":
+                    data = context.Database.SqlQuery<WishList>("select * from " + tableName).ToList();
+                break;
+                default:
+                    data = null;
+                break;
+            }
             return View();
         }
 
